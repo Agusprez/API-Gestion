4a0b9bca7119608ad5f195105695eae45f2c3626
Perez Agustin Gabriel <perezagustin95@hotmail.com>
2024-03-14 18:24:51 -0300

    Logica para haccer la relacion entre usuario y uf

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -24,7 +24,30 @@ const usuariosController = {
       console.error('Error al obtener documentos:', error);
       res.status(500).send('Error interno del servidor.');
     }
+  }, relacionUserUF: async (req, res) => {
+    const { idUsuario, estadoNuevo } = req.body
+
+    try {
+      const querySnapshot = await db.collection('Usuarios').get()
+
+      /*       if (querySnapshot.empty) {
+              console.log('No hay documentos en la colección.');
+              return res.status(404).send('No hay documentos en la colección.');
+            } */
+      const usuario = querySnapshot.docs.find(doc => doc.id === idUsuario)
+      if (!usuario) {
+        console.log(`El usuario con ID ${idUsuario} no fue encontrado.`);
+        return res.status(404).send(`El usuario con ID ${idUsuario} no fue encontrado.`);
+      }
+      // Actualizar el estado del usuario con el nuevo valor
+      await db.collection('Usuarios').doc(idUsuario).update({ ufAsociadaHabilitada: estadoNuevo });
+      res.status(200).send('Usuario actualizado correctamente');
+    } catch (error) {
+      console.error('Error al actualizar el usuario:', error);
+      res.status(500).send('Error al actualizar el usuario');
+    }
   }
+
 };
 
 
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usuariosController = require("../controllers/usuarios.controller");
 
 router.get("/", usuariosController.listarUsuarios)
+router.patch("/relacion-USER-UF", usuariosController.relacionUserUF)
 
 module.exports = router
